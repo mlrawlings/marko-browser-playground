e4a741cd65e884114aa186205de9426bada664a0
Michael Rawlings <ml.rawlings@gmail.com>
2016-05-22 17:07:02 -0700

    rebuild

--- marko-browser.js
+++ marko-browser.js
@@ -725,66 +725,6 @@ https://github.com/joyent/node/blob/master/lib/module.js
     }
 })();
 
-$rmod.def("/source", function(require, exports, module, __filename, __dirname) { var runtime = require('marko');
-var compiler = require('marko/compiler');
-var domready = require('domready');
-var widgets = require('marko-widgets');
-
-window.marko = {
-    templates:{},
-    defineComponent:widgets.defineComponent,
-    ready:domready,
-    compile:(name, src) {
-        if(!name) {
-            throw new Error('A name must be defined for this template');
-        }
-
-        if(name in window.marko.templates) {
-            throw new Error('A template with that name has already been registered');
-        }
-
-        var compiledSrc = compiler.compile(replaceIncludes(src), name, null);
-        var template = evalCommonJsTemplateSrc(name, compiledSrc);
-        window.marko.templates[name] = template;
-        return template;
-    }
-}
-
-function replaceIncludes(src) {
-    return src.replace(/<include\(("[^"]+"|'[^']+')\)/g, '<include(window.marko.templates[$1])');
-}
-
-function evalCommonJsTemplateSrc(name, src) {
-    var wrappedSource = '(function(require, exports, module, __filename, __dirname) { ' + src + ' })';
-    var factoryFunc = eval(wrappedSource);
-    var templateExports = {};
-    var templateModule = {
-        require: require,
-        exports: templateExports,
-        id: name
-    };
-
-    factoryFunc(require, templateExports, templateModule, '/'+name+'.marko', '/');
-    return templateModule.exports;
-}
-
-domready(function() {
-    [].forEach.call(document.querySelectorAll('script[type*=marko]'), function(script) {
-        var src = script.innerHTML;
-        var name = script.getAttribute('name');
-
-        if(!name) {
-            if(window.console) {
-                return console.error('One of your templates defined in a script tag is missing a name attribute');
-            }
-            return document.write('<div style="font-weight:bold;color:#900;">One of your templates defined in a script tag is missing a name attribute</div>');
-        }
-
-        window.marko.compile(name, src);
-    });
-});
-});
-$rmod.run("/source");
 $rmod.main("/marko@3.3.0", "runtime/marko-runtime");
 $rmod.dep("", "marko", "3.3.0");
 $rmod.main("/async-writer@1.4.1", "lib/async-writer");
@@ -27317,6 +27257,40 @@ exports.clearCaches = function() {
 };
 */
 });
+$rmod.main("/domready@1.0.8", "ready");
+$rmod.dep("", "domready", "1.0.8");
+$rmod.def("/domready@1.0.8/ready", function(require, exports, module, __filename, __dirname) { /*!
+  * domready (c) Dustin Diaz 2014 - License MIT
+  */
+!function (name, definition) {
+
+  if (typeof module != 'undefined') module.exports = definition()
+  else if (typeof define == 'function' && typeof define.amd == 'object') define(definition)
+  else this[name] = definition()
+
+}('domready', function () {
+
+  var fns = [], listener
+    , doc = document
+    , hack = doc.documentElement.doScroll
+    , domContentLoaded = 'DOMContentLoaded'
+    , loaded = (hack ? /^loaded|^c/ : /^loaded|^i|^c/).test(doc.readyState)
+
+
+  if (!loaded)
+  doc.addEventListener(domContentLoaded, listener = function () {
+    doc.removeEventListener(domContentLoaded, listener)
+    loaded = 1
+    while (listener = fns.shift()) listener()
+  })
+
+  return function (fn) {
+    loaded ? setTimeout(fn, 0) : fns.push(fn)
+  }
+
+});
+
+});
 $rmod.main("/marko-widgets@6.1.0", "lib/index");
 $rmod.dep("", "marko-widgets", "6.1.0");
 $rmod.def("/marko-widgets@6.1.0/lib/client-init", function(require, exports, module, __filename, __dirname) { /*
@@ -30897,37 +30871,63 @@ exports.onAfterUpdate = updateManager.onAfterUpdate;
 window.$MARKO_WIDGETS = exports; // Helpful when debugging... WARNING: DO NOT USE IN REAL CODE!
 
 });
-$rmod.main("/domready@1.0.8", "ready");
-$rmod.dep("", "domready", "1.0.8");
-$rmod.def("/domready@1.0.8/ready", function(require, exports, module, __filename, __dirname) { /*!
-  * domready (c) Dustin Diaz 2014 - License MIT
-  */
-!function (name, definition) {
+$rmod.def("/source", function(require, exports, module, __filename, __dirname) { var runtime = require('/$/marko'/*'marko'*/);
+var compiler = require('/$/marko/compiler'/*'marko/compiler'*/);
+var domready = require('/$/domready'/*'domready'*/);
+var widgets = require('/$/marko-widgets'/*'marko-widgets'*/);
 
-  if (typeof module != 'undefined') module.exports = definition()
-  else if (typeof define == 'function' && typeof define.amd == 'object') define(definition)
-  else this[name] = definition()
+window.marko = {
+    templates:{},
+    defineComponent:widgets.defineComponent,
+    ready:domready,
+    compile:function(name, src) {
+        if(!name) {
+            throw new Error('A name must be defined for this template');
+        }
 
-}('domready', function () {
+        if(name in window.marko.templates) {
+            throw new Error('A template with that name has already been registered');
+        }
 
-  var fns = [], listener
-    , doc = document
-    , hack = doc.documentElement.doScroll
-    , domContentLoaded = 'DOMContentLoaded'
-    , loaded = (hack ? /^loaded|^c/ : /^loaded|^i|^c/).test(doc.readyState)
+        var compiledSrc = compiler.compile(replaceIncludes(src), name, null);
+        var template = evalCommonJsTemplateSrc(name, compiledSrc);
+        window.marko.templates[name] = template;
+        return template;
+    }
+}
 
+function replaceIncludes(src) {
+    return src.replace(/<include\(("[^"]+"|'[^']+')\)/g, '<include(window.marko.templates[$1])');
+}
 
-  if (!loaded)
-  doc.addEventListener(domContentLoaded, listener = function () {
-    doc.removeEventListener(domContentLoaded, listener)
-    loaded = 1
-    while (listener = fns.shift()) listener()
-  })
+function evalCommonJsTemplateSrc(name, src) {
+    var wrappedSource = '(function(require, exports, module, __filename, __dirname) { ' + src + ' })';
+    var factoryFunc = eval(wrappedSource);
+    var templateExports = {};
+    var templateModule = {
+        require: require,
+        exports: templateExports,
+        id: name
+    };
 
-  return function (fn) {
-    loaded ? setTimeout(fn, 0) : fns.push(fn)
-  }
+    factoryFunc(require, templateExports, templateModule, '/'+name+'.marko', '/');
+    return templateModule.exports;
+}
 
+domready(function() {
+    [].forEach.call(document.querySelectorAll('script[type*=marko]'), function(script) {
+        var src = script.innerHTML;
+        var name = script.getAttribute('name');
+
+        if(!name) {
+            if(window.console) {
+                return console.error('One of your templates defined in a script tag is missing a name attribute');
+            }
+            return document.write('<div style="font-weight:bold;color:#900;">One of your templates defined in a script tag is missing a name attribute</div>');
+        }
+
+        window.marko.compile(name, src);
+    });
 });
-
 });
+$rmod.run("/source");
